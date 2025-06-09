ca7069f9d9ab655e8ea1b99b6fe88e8f60e4e62f
Userton <104372373+Userton@users.noreply.github.com>
2025-06-09 21:38:20 +0800

    Initial commit

---
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeDemo.Data;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", " 類別名稱不能跟顯示順序一致 ");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ TeaTimeDemo/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
+{
+    public class Category
+    {
+        [Key]
+        public int? Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("類別名稱")]
+        public string? Name { get; set; }
+        [DisplayName("顯示順序")]
+        [Range(1, 100, ErrorMessage = "顯示順序必須介於 1 到 100 之間。")]
+        public int? DisplayOrder { get; set; }
+    }
+}
