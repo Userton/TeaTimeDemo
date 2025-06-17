ffa8798e8cb8613a908b1962a61734a0481644ed
Userton <104372373+Userton@users.noreply.github.com>
2025-06-17 18:46:38 +0800

    06171845
    06171845拆 分

--- TeaTimeDemo/Controllers/HomeController.cs
+++ TeaTimeDemo/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using TeaTimeDemo.DataAccess.Migrations;
+using TeaTimeDemo.DataAccess;
 using TeaTimeDemo.Models;
 
 namespace TeaTimeDemo.Controllers
